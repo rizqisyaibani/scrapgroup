10f56c8b01bdc7436cc533e9fa1bc259e4e723a5
Rizqi Syaibani <muhammad1188ft.2019@student.uny.ac.id>
2022-01-13 15:11:03 +0700

    menambahkan login dan connect mysql

--- app.py
+++ app.py
@@ -1,129 +1,92 @@
-from flask import Flask, render_template, request, url_for, flash, redirect
+from flask import Flask, render_template, request, url_for, flash, redirect, session
+from flask_mysqldb import MySQL
 from werkzeug.exceptions import abort
 
+import MySQLdb.cursors
 import mysql.connector
+import re
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
-def get_db_connection():
-    mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database="scrape"
-    )
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'scrape'
 
-    return mydb
-
-def get_post(post_id):
-    mydb = get_db_connection()
-    cursor = mydb.cursor()
-
-    cursor.execute(
-        'SELECT * FROM posts WHERE id = %s',
-        (post_id,)
-    )
-
-    result = cursor.fetchone()
-    post = {
-        'id': result[0],
-        'created': result[1],
-        'title': result[2],
-        'content': result[3]
-    }
-    mydb.close()
-
-    if post is None:
-        abort(404)
-    return post
+mysql = MySQL(app)
 
 
-@app.route('/')
-def index():
-    mydb = get_db_connection()
-    cursor = mydb.cursor()
-    cursor.execute('SELECT * FROM book')
-    result = cursor.fetchall()
-    book = []
-    for entry in result:
-        record = {
-            'cover': entry[0],
-            'title': entry[1],
-            'descr': entry[2],
-            'author': entry[3],
-            'publisher': entry[4],
-            'pub_date': entry[5],
-            'genres': entry[6],
-            'lang': entry[7],
-            'pages': entry[8],
-            'comp': entry[9],
-            'price': entry[10],
-            'rating': entry[11],
-            'tot_rat': entry[12]
-        }
-        book.append(record)
-    mydb.close()
-    
-    return render_template('index.html')
-    
-
-@app.route('/<int:post_id>')
-def post(post_id):
-    post = get_post(post_id)
-    return render_template('post.html', post=post)
-
-@app.route('/create', methods=('GET', 'POST'))
-def create():
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
-
-        if not title:
-            flash('Title is required!')
-        else:
-            mydb = get_db_connection()
-            cursor = mydb.cursor()
-            cursor.execute('INSERT INTO posts (title, content) VALUES (%s, %s)',
-                         (title, content))
-            mydb.commit()
-            mydb.close()
-            return redirect(url_for('index'))
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
         
-    return render_template('create.html')
-
-@app.route('/<int:id>/edit', methods=('GET', 'POST'))
-def edit(id):
-    post = get_post(id)
-
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
-
-        if not title:
-            flash('Title is required!')
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            
+            return redirect(url_for('home'))
         else:
-            mydb = get_db_connection()
-            cursor = mydb.cursor()
-            cursor.execute('UPDATE posts SET title = %s, content = %s'
-                         ' WHERE id = %s',
-                         (title, content, id))
-            mydb.commit()
-            mydb.close()
-            return redirect(url_for('index'))
+            msg = 'Incorrect username/password!'
+    return render_template('index.html', msg='')
 
-    return render_template('edit.html', post=post)
+@app.route('/logout')
+def logout():
+   session.pop('loggedin', None)
+   session.pop('id', None)
+   session.pop('username', None)
+   return redirect(url_for('login'))
 
-@app.route('/<int:id>/delete', methods=('POST',))
-def delete(id):
-    post = get_post(id)
-    mydb = get_db_connection()
-    cursor = mydb.cursor()
-    cursor.execute('DELETE FROM posts WHERE id = %s', (id,))
-    mydb.commit()
-    mydb.close()
-    flash('"{}" was successfully deleted!'.format(post['title']))
-    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+    elif request.method == 'POST':
+        msg = 'Please fill out the form!'
+    return render_template('register.html', msg=msg)
+
+@app.route('/home')
+def home():
+    if 'loggedin' in session:
+        return render_template('home.html', username=session['username'])
+    return redirect(url_for('login'))
+
+@app.route('/profile')
+def profile():
+    if 'loggedin' in session:
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE id = %s', (session['id'],))
+        account = cursor.fetchone()
+        return render_template('profile.html', account=account)
+    return redirect(url_for('login'))
+
 
 @app.route('/admin')
 def admin():
@@ -139,7 +102,7 @@ def admin():
                          (title, content))
             mydb.commit()
             mydb.close()
-            return redirect(url_for('index'))
+            return redirect(url_for('home'))
         
     return render_template('admin.html')
 
--- schema.sql
+++ schema.sql
@@ -1,17 +1,27 @@
 DROP TABLE IF EXISTS book;
 
-CREATE TABLE book (
-    cover VARCHAR NOT NULL,
-    title VARCHAR NOT NULL,
-    descr VARCHAR NOT NULL,
-    author VARCHAR NOT NULL,
-    publisher VARCHAR NOT NULL,
-    pub_date DATE NOT NULL,
-    genres VARCHAR NOT NULL,
-    lang VARCHAR NOT NULL,
-    pages INT NOT NULL,
-    comp VARCHAR NOT NULL,
-    price DECIMAL NOT NULL,
-    rating FLOAT NOT NULL,
-    tot_rat INT NOT NULL
-);
+CREATE TABLE IF NOT EXISTS `books` (
+    `cover` varchar NOT NULL,
+    `title` varchar(50) NOT NULL,
+    `descr` varchar(255) NOT NULL,
+    `author` varchar(100) NOT NULL,
+    `publisher` varchar(100) NOT NULL,
+    `pub_date` date NOT NULL,
+    `genres` varchar(100) NOT NULL,
+    `lang` varchar(50) NOT NULL,
+    `pages` int(5) NOT NULL,
+    `comp` varchar(100) NOT NULL,
+    `price` decimal(20) NOT NULL,
+    `rating` float(5) NOT NULL,
+    `tot_rat` int(10) NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS `accounts` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+  	`username` varchar(50) NOT NULL,
+  	`password` varchar(255) NOT NULL,
+  	`email` varchar(100) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+INSERT INTO `accounts` (`id`, `username`, `password`, `email`) VALUES (1, 'test', 'test', 'test@test.com');
