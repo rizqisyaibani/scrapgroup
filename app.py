316e921af623aee351aa3ee14f62cf677ba88e16
larasatilatifah <larasati1838ft.2019@student.uny.ac.id>
2022-01-19 00:32:01 +0700

    login google oauth

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template, request, url_for, flash, redirect, session
 from flask_mysqldb import MySQL
 from werkzeug.exceptions import abort
+from google.oauth2 import id_token
+from google_auth_oauthlib.flow import Flow
+from pip._vendor import cachecontrol
+import google.auth.transport.requests
+
 from bs4 import BeautifulSoup
-
-
 from web_scrap import scrape_func
+
 import requests
 import MySQLdb.cursors
 import mysql.connector
 import re
+
+import os
+import pathlib
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
@@ -17,6 +24,28 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'scrape'
+
+app.secret_key = "scrapgroup.com"
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+GOOGLE_CLIENT_ID = "102269209906-qsk38k6jb0ij46iqu0a8f7efp4a61ok6.apps.googleusercontent.com"
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="http://127.0.0.1:5000/callback"
+)
+
+# def login_is_required(function):
+#     def wrapper(*args, **kwargs):
+#         if "google_id" not in session:
+#             return abort(401)  # Authorization required
+#         else:
+#             return function()
+
+#     return wrapper
 
 mysql = MySQL(app)
 
@@ -40,12 +69,43 @@ def login():
             msg = 'Incorrect username/password!'
     return render_template('index.html', msg='')
 
+@app.route('/googlelogin')
+def googlelogin():
+    authorization_url, state = flow.authorization_url()
+    session["state"] = state
+    return redirect(authorization_url)
+
 @app.route('/logout')
 def logout():
    session.pop('loggedin', None)
    session.pop('id', None)
    session.pop('username', None)
    return redirect(url_for('login'))
+
+@app.route("/callback")
+def callback():
+    # msg = ''
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session["state"] == request.args["state"]:
+        abort(500)  # State does not match!
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+
+    session["google_id"] = id_info.get("sub")
+    session["name"] = id_info.get("name")
+    # return redirect("/protected_area")
+    # return render_template('index.html', msg='')
+    return redirect(url_for('protected_area'))
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -103,7 +163,7 @@ def profile():
 def book():
     return render_template('book.html')
 
-@app.route('/book/scrape', methods=('GET', 'POST'))
+@app.route('/book/scrape', methods=['GET', 'POST'])
 def scrape():
     if request.method == 'POST' and 'link' in request.form:
         link = request.form['link']
@@ -123,15 +183,30 @@ def scrape():
     return render_template('scrape.html')
     
     
-@app.route('/book/scrape/report', methods=('GET', 'POST'))
+@app.route('/book/scrape/report', methods=['GET', 'POST'])
 def report():
     return render_template('scrape_report.html')
 
 
-@app.route('/book/bookmenu', methods=('POST',))
+@app.route('/book/bookmenu', methods=['POST',])
 def bookmenu():
     return render_template('book_menu.html')
 
-@app.route('/book/dashboard', methods=('POST',))
+@app.route('/book/dashboard', methods=['POST',])
 def dashboard():
     return render_template('dashboard.html')
+
+@app.route("/protected_area")
+# @login_is_required
+def protected_area():
+    # if 'loggedin' in session:
+    #     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    #     cursor.execute('SELECT * FROM accounts WHERE id = %s', (session['id'],))
+    #     account = cursor.fetchone()
+    #     return render_template('profile.html', account=account)
+    # return redirect(url_for('home'))
+    # return render_template('home.html', username=session['username'])
+    return render_template('home.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
